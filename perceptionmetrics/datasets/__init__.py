fd810f7e151c19ef2b3ffbe1bdb968062179653d
T.S.  Stanley <140648660+tejasstanley@users.noreply.github.com>
2026-05-27 18:22:40 +0200

    Add Cityscapes dataset adapter (#577)
    * Add Citscapes dataset adapter
    
    * Add Cityscapes dataset adapter and cleanup
    
    * Update datasets/__init__.py
    
    * Correction in build_dataset arg
    
    * Add helper translation function
    
    * Black formatting, add cityscapescripts dependency
    
    * Remove poetry lock file
    
    * Revert pyproject dependency change
    
    * Restore cityscacpescripts dependency

--- perceptionmetrics/datasets/__init__.py
+++ perceptionmetrics/datasets/__init__.py
@@ -20,11 +20,14 @@ from perceptionmetrics.datasets.rellis3d import (
 )
 from perceptionmetrics.datasets.rugd import RUGDImageSegmentationDataset
 from perceptionmetrics.datasets.wildscenes import WildscenesImageSegmentationDataset
+from perceptionmetrics.datasets.cityscapes import CityscapesImageSegmentationDataset
+
 try:
     from perceptionmetrics.datasets.coco import CocoDataset
 except ImportError:
     print("COCO dataset dependencies not available")
     CocoDataset = None
+
 
 REGISTRY = {
     "gaia_image_segmentation": GaiaImageSegmentationDataset,
@@ -37,6 +40,7 @@ REGISTRY = {
     "rellis3d_lidar_segmentation": Rellis3DLiDARSegmentationDataset,
     "rugd_image_segmentation": RUGDImageSegmentationDataset,
     "wildscenes_image_segmentation": WildscenesImageSegmentationDataset,
+    "cityscapes_image_segmentation": CityscapesImageSegmentationDataset,
     "nuimages_image_segmentation": NuImagesSegmentationDataset,
     "nuimages_detection": NuImagesDetectionDataset,
 }
